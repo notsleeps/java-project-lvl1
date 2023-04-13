f6a55e45a8d7d74eaa3140c7eeff041772458cc1
Evgenii Chernyshov <zidane98@mail.ru>
2023-04-13 16:46:43 +0300

    update

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -3,7 +3,7 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Prime {
-    private static final int A = 1 + Engine.randomNumber10();
+    private static final int A = 1 + Engine.randomNumber100();
     private static final int B = 1 + Engine.randomNumber100();
     private static final int C = 1 + Engine.randomNumber100();
     public static void primeNumber() {
